75872d636bc30fa9188a837dcd5fcc4132d417a9
rushabh gaherwar <rushabh.gaherwar@compass.com>
2020-11-27 11:43:07 +0530

    added config file & loginTest class

---
+++ src/test/java/loginTest/LoginApiTests.java
@@ -0,0 +1,12 @@
+package loginTest;
+
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class LoginApiTests {
+
+
+}
